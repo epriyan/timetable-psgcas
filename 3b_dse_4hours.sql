507bbb4dda16f9e183197fa8a32a94ebcd7b231f
Priyan E <117143367+epriyan@users.noreply.github.com>
2024-12-03 00:57:30 +0530

    Add files via upload

---
+++ 3b_dse_4hours.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 28, 2024 at 01:29 PM
+-- Server version: 8.0.39
+-- PHP Version: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `timetable`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `3b_dse_4hours`
+--
+
+DROP TABLE IF EXISTS `3b_dse_4hours`;
+CREATE TABLE IF NOT EXISTS `3b_dse_4hours` (
+  `TID` varchar(10) NOT NULL,
+  `Teacher_Name` varchar(50) NOT NULL,
+  `Subject` varchar(50) NOT NULL,
+  `Hours` int NOT NULL,
+  `Class` varchar(20) NOT NULL,
+  `SPID` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `3b_dse_4hours`
+--
+
+INSERT INTO `3b_dse_4hours` (`TID`, `Teacher_Name`, `Subject`, `Hours`, `Class`, `SPID`) VALUES
+('TDSE', 'DSE Staff', 'DSE', 4, '3B', '3BDSE');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
